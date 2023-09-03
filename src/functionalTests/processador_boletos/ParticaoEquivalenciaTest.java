c7a9ffb071b1c0edfa89e3c3147e096dde5265e0
hiarlyfs <hiarly.souto@ccc.ufcg.edu.br>
2023-09-03 06:51:50 -0300

    Create test to ProcessadorBoleto

---
+++ src/functionalTests/processador_boletos/ParticaoEquivalenciaTest.java
@@ -0,0 +1,47 @@
+package functionalTests.processador_boletos;
+
+import main.processador_boletos.ProcessadorBoleto;
+import main.processador_boletos.entity.Boleto;
+import main.processador_boletos.entity.Fatura;
+import main.processador_boletos.entity.Pagamento;
+import main.processador_boletos.enums.FaturaStatusEnum;
+import main.processador_boletos.enums.PaymentTypeEnum;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class ParticaoEquivalenciaTest {
+
+    @Test
+    @DisplayName("Deve colocar fatura marcada como PAGA e nenhum pagamento criado")
+    void test1() {
+        Boleto boleto1 = new Boleto("1", LocalDate.now(), 400);
+        Boleto boleto2 = new Boleto("2", LocalDate.now(), 500);
+        Fatura fatura = new Fatura(LocalDate.now(), 1000, "Marcos da Night");
+        ProcessadorBoleto processadorBoleto = new ProcessadorBoleto(fatura, List.of(boleto1, boleto2));
+        processadorBoleto.process();
+
+        Assertions.assertEquals(fatura.getStatus(), FaturaStatusEnum.NOT_PAID);
+        Assertions.assertNull(boleto1.getPayment());
+        Assertions.assertNull(boleto2.getPayment());
+    }
+
+    @Test
+    @DisplayName("Deve colocar Fatura marcada como PAGA e três pagamentos do tipo BOLETO criados.")
+    void test2() {
+        Boleto boleto1 = new Boleto("1", LocalDate.now(), 400);
+        Boleto boleto2 = new Boleto("2", LocalDate.now(), 500);
+        Boleto boleto3 = new Boleto("2", LocalDate.now(), 600);
+        Fatura fatura = new Fatura(LocalDate.now(), 1500, "Marcos da Night");
+        ProcessadorBoleto processadorBoleto = new ProcessadorBoleto(fatura, List.of(boleto1, boleto2, boleto3));
+        processadorBoleto.process();
+
+        Assertions.assertEquals(fatura.getStatus(), FaturaStatusEnum.PAID);
+        Assertions.assertEquals(boleto1.getPayment(), new Pagamento(boleto1.getValue(), boleto1.getDate(), PaymentTypeEnum.BOLETO));
+        Assertions.assertEquals(boleto2.getPayment(), new Pagamento(boleto2.getValue(), boleto2.getDate(), PaymentTypeEnum.BOLETO));
+        Assertions.assertEquals(boleto3.getPayment(), new Pagamento(boleto3.getValue(), boleto3.getDate(), PaymentTypeEnum.BOLETO));
+    }
+}
--- src/functionalTests/processador_boletos/TabelaDecisaoTest.java
+++ src/functionalTests/processador_boletos/TabelaDecisaoTest.java
@@ -1,4 +1,4 @@
-package tests.processador_boletos;
+package functionalTests.processador_boletos;
 
 import main.processador_boletos.ProcessadorBoleto;
 import main.processador_boletos.entity.Boleto;
@@ -13,14 +13,28 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.util.List;
 
-public class ProcessadorBoletosTest {
+public class TabelaDecisaoTest {
 
     @Test
-    @DisplayName("should pay fatura when fatura value is equal than all Boletos' values")
-    void shouldPayFaturaWhenFaturaValueIsEqualThanBoletosValue() {
+    @DisplayName("Deve colocar Fatura marcada como PAGA e dois pagamentos do tipo BOLETO criados.")
+    void test1() {
+        Boleto boleto1 = new Boleto("1", LocalDate.now(), 1200);
+        Boleto boleto2 = new Boleto("2", LocalDate.now(), 500);
+        Fatura fatura = new Fatura(LocalDate.now(), 1500, "Marcos da Night");
+        ProcessadorBoleto processadorBoleto = new ProcessadorBoleto(fatura, List.of(boleto1, boleto2));
+        processadorBoleto.process();
+
+        Assertions.assertEquals(fatura.getStatus(), FaturaStatusEnum.PAID);
+        Assertions.assertEquals(boleto1.getPayment(), new Pagamento(boleto1.getValue(), boleto1.getDate(), PaymentTypeEnum.BOLETO));
+        Assertions.assertEquals(boleto2.getPayment(), new Pagamento(boleto2.getValue(), boleto2.getDate(), PaymentTypeEnum.BOLETO));
+    }
+
+    @Test
+    @DisplayName("Deve colocar Fatura marcada como PAGA e três pagamentos do tipo BOLETO criados.")
+    void test2() {
         Boleto boleto1 = new Boleto("1", LocalDate.now(), 500);
         Boleto boleto2 = new Boleto("2", LocalDate.now(), 400);
-        Boleto boleto3 = new Boleto("1", LocalDate.now(), 600);
+        Boleto boleto3 = new Boleto("2", LocalDate.now(), 600);
         Fatura fatura = new Fatura(LocalDate.now(), 1500, "Marcos da Night");
         ProcessadorBoleto processadorBoleto = new ProcessadorBoleto(fatura, List.of(boleto1, boleto2, boleto3));
         processadorBoleto.process();
@@ -32,35 +46,17 @@ public class ProcessadorBoletosTest {
     }
 
     @Test
-    @DisplayName("should pay fatura when fatura value is less than All Boletos' values")
-    void shouldPayFaturaWhenFaturaValueIsLessThanBoletosValue() {
-        Boleto boleto1 = new Boleto("1", LocalDate.now(), 500);
-        Boleto boleto2 = new Boleto("2", LocalDate.now(), 400);
-        Boleto boleto3 = new Boleto("1", LocalDate.now(), 600);
-        Fatura fatura = new Fatura(LocalDate.now(), 1000, "Marcos da Night");
-        ProcessadorBoleto processadorBoleto = new ProcessadorBoleto(fatura, List.of(boleto1, boleto2, boleto3));
-        processadorBoleto.process();
-
-        Assertions.assertEquals(fatura.getStatus(), FaturaStatusEnum.PAID);
-        Assertions.assertEquals(boleto1.getPayment(), new Pagamento(boleto1.getValue(), boleto1.getDate(), PaymentTypeEnum.BOLETO));
-        Assertions.assertEquals(boleto2.getPayment(), new Pagamento(boleto2.getValue(), boleto2.getDate(), PaymentTypeEnum.BOLETO));
-        Assertions.assertEquals(boleto3.getPayment(), new Pagamento(boleto3.getValue(), boleto3.getDate(), PaymentTypeEnum.BOLETO));
-    }
-
-    @Test
-    @DisplayName("should not pay fatura when fatura value is greater than all Boletos' values")
-    void shouldNotPayFaturaWhenFaturaValueIsGreaterThanBoletosValue() {
-        Boleto boleto1 = new Boleto("1", LocalDate.now(), 500);
-        Boleto boleto2 = new Boleto("2", LocalDate.now(), 400);
-        Boleto boleto3 = new Boleto("1", LocalDate.now(), 600);
-        Fatura fatura = new Fatura(LocalDate.now(), 2000, "Marcos da Night");
-        ProcessadorBoleto processadorBoleto = new ProcessadorBoleto(fatura, List.of(boleto1, boleto2, boleto3));
+    @DisplayName("Deve colocar Fatura marcada como NÃO PAGA e nenhuma pagamento do tipo BOLETO criado.")
+    void test3() {
+        Boleto boleto1 = new Boleto("1", LocalDate.now(), 100);
+        Boleto boleto2 = new Boleto("2", LocalDate.now(), 200);
+        Fatura fatura = new Fatura(LocalDate.now(), 500, "Marcos da Night");
+        ProcessadorBoleto processadorBoleto = new ProcessadorBoleto(fatura, List.of(boleto1, boleto2));
         processadorBoleto.process();
 
         Assertions.assertEquals(fatura.getStatus(), FaturaStatusEnum.NOT_PAID);
         Assertions.assertNull(boleto1.getPayment());
         Assertions.assertNull(boleto2.getPayment());
-        Assertions.assertNull(boleto3.getPayment());
     }
 
 }
